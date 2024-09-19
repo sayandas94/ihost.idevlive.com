1caded227d2adcd6db25d77f9b237cfa8097a5a4
Sayan <sayan.das@idevlive.com>
2024-09-19 18:36:24 +0530

    Tried to use cookies instead of session. Need to work more on this.
    Right now its check if session variable is available or not. Need to add code for checking if the user is authorized with the current session token.

--- app/Http/Middleware/AuthorisedUser.php
+++ app/Http/Middleware/AuthorisedUser.php
@@ -6,20 +6,48 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Cookie;
 
 class AuthorisedUser
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (!$request->session()->get('token')) {
-            $request->session()->put('previous_url', url()->previous());
-            return redirect(url('sign-in'));
-        }
-        return $next($request);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle(Request $request, Closure $next): Response
+	{
+		if (!$request->session()->get('token')) {
+			$request->session()->put('previous_url', url()->previous());
+			return redirect(url('sign-in'));
+		}
+
+		// if (!Cookie::get('auth_token')) {
+		// 	$request->session()->put('previous_url', url()->previous());
+		// 	return redirect(url('sign-in'));
+		// }
+
+		// $curl = curl_init(env('API_URL') . 'accounts/profile');
+
+		// curl_setopt_array($curl, [
+		// 	CURLOPT_RETURNTRANSFER => true,
+		// 	CURLOPT_HTTPHEADER => [
+		// 		'Content-Type: application/json',
+		// 		'Authorization: Bearer ' . Cookie::get('auth_token')
+		// 	]
+		// ]);
+
+		// $response = curl_exec($curl);
+
+		// curl_close($curl);
+
+		// $response = json_decode($response);
+
+		// if (!$response) {
+		// 	$request->session()->put('previous_url', url()->previous());
+		// 	return redirect(url('sign-in'));
+		// }
+
+		return $next($request);
+	}
 }
